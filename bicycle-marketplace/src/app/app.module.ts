beccd0e89f433e41442422104bfb2a8401eb11ae
Sam Jamal <=>
2019-08-06 22:11:18 -0700

    app running

--- bicycle-marketplace/src/app/app.module.ts
+++ bicycle-marketplace/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { BikeMarketService } from './bike-market.service';
-import { CookieService } from 'ngx-cookie-service';
-import { CookieModule } from 'ngx-cookie';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { BrowseComponent } from './browse/browse.component';
-import { ListAllComponent } from './browse/list-all/list-all.component';
-import { ListMyComponent } from './browse/list-my/list-my.component';
-import { SearchPipe } from './filter.pipe';
+import { BikeMarketService } from "./bike-market.service";
+import { CookieService } from "ngx-cookie-service";
+import { CookieModule } from "ngx-cookie";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { BrowseComponent } from "./browse/browse.component";
+import { ListAllComponent } from "./browse/list-all/list-all.component";
+import { ListMyComponent } from "./browse/list-my/list-my.component";
+import { SearchPipe } from "./filter.pipe";
 
 @NgModule({
   declarations: [
@@ -35,4 +35,4 @@ import { SearchPipe } from './filter.pipe';
   providers: [BikeMarketService, CookieService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- bicycle-marketplace/src/app/bike-market.service.ts
+++ bicycle-marketplace/src/app/bike-market.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Http } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie';
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Http } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
+import { CookieService } from "ngx-cookie";
+import "rxjs/add/operator/toPromise";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class BikeMarketService {
@@ -19,78 +19,81 @@ export class BikeMarketService {
     private _http: Http,
     private _http2: HttpClient,
     private _cookieService: CookieService
-  ) { }
+  ) {}
 
-  createUser(user){
-    this._http.post('/new_user', user).subscribe(
-      user => this.current_user.next(user.json()),
-      errorResponse => console.log(errorResponse)
-    );
+  createUser(user) {
+    this._http
+      .post("/new_user", user)
+      .subscribe(
+        user => this.current_user.next(user.json()),
+        errorResponse => console.log(errorResponse)
+      );
   }
-  getUser(user){
-    this._http.get('/get_user/'+user).subscribe(
-      user => {
-        this.contact_info.next(user.json())
-      });
+  getUser(user) {
+    this._http.get("/get_user/" + user).subscribe(user => {
+      this.contact_info.next(user.json());
+    });
   }
-  login(user){
-    this._http.post('/login', user).subscribe(
+  login(user) {
+    this._http.post("/login", user).subscribe(
       user => {
-        if(user==null){
-          this.login_errors = 'email or password is incorrect';
+        if (user == null) {
+          this.login_errors = "email or password is incorrect";
         }
-        this.current_user.next(user.json())
+        this.current_user.next(user.json());
       },
       errorResponse => console.log(errorResponse)
     );
   }
-  logout(){
-    this.login_errors = '';
-    this._http.delete('/logout').subscribe(
-      response => this.current_user.next(response.json())
-    );
+  logout() {
+    this.login_errors = "";
+    this._http
+      .delete("/logout")
+      .subscribe(response => this.current_user.next(response.json()));
   }
   logged_in(): boolean {
-    const session = this._cookieService.get('session');
-    const userID = this._cookieService.get('userID');
-    const expiration = parseInt(this._cookieService.get('expiration'), 10);
+    const session = this._cookieService.get("session");
+    const userID = this._cookieService.get("userID");
+    const expiration = parseInt(this._cookieService.get("expiration"), 10);
 
     return session && expiration && userID && expiration > Date.now();
   }
 
-  createBike(data){
-    this._http.post('/new_bike/'+data.user_id, data).subscribe(
-      bikes => {
-        console.log('added bike')
-        this.retrieveMyBikes(data.user_id)
-      })
+  createBike(data) {
+    this._http.post("/new_bike/" + data.user_id, data).subscribe(bikes => {
+      console.log("added bike");
+      this.retrieveMyBikes(data.user_id);
+    });
   }
   retrieveBikes() {
-    this._http.get('/bikes').subscribe(
-      bikes => this.bikes.next(bikes.json()),
-      errorResponse => console.log(errorResponse)
-    );
+    this._http
+      .get("/bikes")
+      .subscribe(
+        bikes => this.bikes.next(bikes.json()),
+        errorResponse => console.log(errorResponse)
+      );
   }
   retrieveMyBikes(id) {
-    console.log('grabbing my bikes...')
-    this._http.get('/my_bikes/'+id).subscribe(
-      bikes => this.my_bikes.next(bikes.json()),
-      errorResponse => console.log(errorResponse)
-    );
+    console.log("grabbing my bikes...");
+    this._http
+      .get("/my_bikes/" + id)
+      .subscribe(
+        bikes => this.my_bikes.next(bikes.json()),
+        errorResponse => console.log(errorResponse)
+      );
   }
-  updateBike(data){
-    this._http.post('/update_bike/'+data.bike_id, data.new_bike).subscribe(
-      response => {
-        console.log('updated bike')
-        this.retrieveMyBikes(this._cookieService.get('userID'))
-      }
-    )
+  updateBike(data) {
+    this._http
+      .post("/update_bike/" + data.bike_id, data.new_bike)
+      .subscribe(response => {
+        console.log("updated bike");
+        this.retrieveMyBikes(this._cookieService.get("userID"));
+      });
   }
-  deleteBike(data){
-    this._http.post('/delete_bike/'+data.bike_id, data).subscribe(
-      bikes => {
-        this.retrieveBikes();
-        this.retrieveMyBikes(data.user_id);
-      })
+  deleteBike(data) {
+    this._http.post("/delete_bike/" + data.bike_id, data).subscribe(bikes => {
+      this.retrieveBikes();
+      this.retrieveMyBikes(data.user_id);
+    });
   }
 }
