4455d4bb6c7f75d09747f980949b0bfd0307c24d
Sam Jamal <=>
2019-08-07 02:50:44 -0700

    app builds and works properly in local dev

--- bicycle-marketplace/src/app/browse/list-my/list-my.component.spec.ts
+++ bicycle-marketplace/src/app/browse/list-my/list-my.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-import { ListMyComponent } from './list-my.component';
+import { ListMyComponent } from "./list-my.component";
 
-describe('ListMyComponent', () => {
+describe("ListMyComponent", () => {
   let component: ListMyComponent;
   let fixture: ComponentFixture<ListMyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListMyComponent ]
-    })
-    .compileComponents();
+      declarations: [ListMyComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +20,7 @@ describe('ListMyComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
